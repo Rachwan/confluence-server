3af64bb9a17319c93ae9616a74f6c966ffd01608
Rachwan <rachwan.harb2023@gmail.com>
2024-02-12 14:31:55 +0200

    Add uploadImage.fields in the user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,45 @@
 import express from "express";
 import userController from "../controllers/userControllers.js";
-import { verifyToken, checkRole } from '../middleware/authentication.js';
+import { verifyToken, checkRole } from "../middleware/authentication.js";
 import uploadImage from "../middleware/multer.js";
 
 export const userRoutes = express.Router();
 
-userRoutes.post('/register',
-uploadImage.fields([
-  { name: "background", maxCount: 1 },
-  { name: "profile", maxCount: 1 },
-]), userController.register);
+userRoutes.post(
+  "/register",
+  uploadImage.fields([
+    { name: "background", maxCount: 1 },
+    { name: "profile", maxCount: 1 },
+  ]),
+  userController.register
+);
 
-userRoutes.get('/all', verifyToken, checkRole(["admin"]), userController.getAllUsers);
-userRoutes.get('/:id', verifyToken, userController.getUserById);
-userRoutes.put('/:id', verifyToken, userController.updateUserById);
-userRoutes.delete('/:id', verifyToken, checkRole(["admin"]), userController.deleteUserById);
+userRoutes.get(
+  "/all",
+  verifyToken,
+  checkRole(["admin"]),
+  userController.getAllUsers
+);
 
-userRoutes.get('/read/one', verifyToken, userController.getOneUser);
+userRoutes.get("/:id", verifyToken, userController.getUserById);
+
+userRoutes.put(
+  "/:id",
+  uploadImage.fields([
+    { name: "background", maxCount: 1 },
+    { name: "profile", maxCount: 1 },
+  ]),
+  verifyToken,
+  userController.updateUserById
+);
+
+userRoutes.delete(
+  "/:id",
+  verifyToken,
+  checkRole(["admin"]),
+  userController.deleteUserById
+);
+
+userRoutes.get("/read/one", verifyToken, userController.getOneUser);
 
 export default userRoutes;
