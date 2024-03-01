c208012b2c36061eb0528bb0e0eb5e2e3d2352c3
Rachwan <rachwan.harb2023@gmail.com>
2024-03-01 15:58:14 +0200

    Commented the line added it to cors

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const corsOption = {
   ],
   credentials: true,
   optionsSuccessStatus: 200,
-  exposedHeaders: ["set-cookie"],
+  // exposedHeaders: ["set-cookie"],
 };
 
 app.use(cors(corsOption));
